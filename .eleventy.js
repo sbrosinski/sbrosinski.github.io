6999a7e3130de27d508c4dca214d5ea78564c55b
Stephan Brosinski <s@brosinski.com>
2020-11-08 18:19:06 +0100

    Styling

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,8 @@
+const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
+
 module.exports = function(eleventyConfig) {
-    eleventyConfig.addPassthroughCopy('css')
+    eleventyConfig.addPassthroughCopy('css');
+    eleventyConfig.addPlugin(syntaxHighlight);
     return {
       passthroughFileCopy: true,
       dir: {
---
+++ content/notes/code_note.md
@@ -0,0 +1,16 @@
+---
+title: Node with code
+tags: note
+---
+
+This node contains some Java code:
+
+See description [here](https://www.11ty.dev/docs/plugins/syntaxhighlight/).
+
+```java
+class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!"); 
+    }
+}
+```
